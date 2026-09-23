76f7613d2fa16d3922c173a3e75518a53a711450
Yead191 <yead191@gmail.com>
2026-09-23 14:52:46 +0600

    updated

--- src/helpers/next-fetch/getAccessToken.tsx
+++ src/helpers/next-fetch/getAccessToken.tsx
@@ -2,7 +2,11 @@
 
 import { cookies } from "next/headers";
 
-export const getAccessToken = async () => {
-  const cookieStore = await cookies();
-  return cookieStore.get("accessToken")?.value;
+export const getAccessToken = async (): Promise<string | undefined> => {
+  try {
+    const cookieStore = await cookies();
+    return cookieStore.get("accessToken")?.value;
+  } catch {
+    return undefined;
+  }
 };
